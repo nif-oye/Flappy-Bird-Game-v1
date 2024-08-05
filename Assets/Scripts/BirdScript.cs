e0da12f3bb5f49360b130484d2cbbf3b44276414
Oluwanifemi <oluwanifemi.dev@gmail.com>
2024-08-05 22:50:44 +0100

    User Interface and Game Over

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D birdRigidbody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
     void Start()
     {
         gameObject.name = "Nifemi's Bird"; 
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true){
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive){
             birdRigidbody.velocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
---
+++ Assets/Scripts/PipeMiddleScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PipeMiddleScript : MonoBehaviour
+{
+    public LogicScript logic;
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.layer == 3){
+            logic.addScore(1);
+        }
+    }
+}
